eeee72145626093d12535c774c4d2af71a645972
Winton Davies <wdavies@cs.stanford.edu>
2023-05-23 14:47:16 -0700

    Update __init__.py
    added a statistical method

--- chatspot/__init__.py
+++ chatspot/__init__.py
@@ -1,3 +1,4 @@
 from chatspot.chatspot import login
 from chatspot.chatspot import get_recommendations_by_vibe
 from chatspot.chatspot import get_features
+from chatspot.chatspot import songs_value_range
