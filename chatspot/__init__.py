db74e4df09bcda94e21c55b10308f448533dfb70
Winton Davies <wdavies@cs.stanford.edu>
2023-05-27 22:19:30 -0700

    Update __init__.py

--- chatspot/__init__.py
+++ chatspot/__init__.py
@@ -3,3 +3,5 @@ from chatspot.chatspot import get_recommendations_by_vibe
 from chatspot.chatspot import get_features
 from chatspot.chatspot import songs_value_range
 from chatspot.chatspot import make_top_genre_list
+from chatspot.chatspot import plotTSNE
+from chatspot.chatspot import pca
