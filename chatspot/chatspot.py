da1af5b3237a1e3ae353952b8c1a0dfecf4beb39
Winton Davies <wdavies@cs.stanford.edu>
2023-05-27 22:04:35 -0700

    Update chatspot.py
    Added PCA code for visualization

--- chatspot/chatspot.py
+++ chatspot/chatspot.py
@@ -6,9 +6,21 @@ import time
 import json
 import yaml
 import numpy as np 
+from numpy import reshape
 import scipy.stats as st
 import re
 from collections import Counter
+import math
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+
+
 
 def login(spotify_client_id, spotify_client_secret, openai_api_key):
    spotify_credentials = SpotifyClientCredentials(client_id=spotify_client_id, 
@@ -212,3 +224,141 @@ def songs_value_range(songs,ignore = []):
     data = f_list[f]
     f_list[f] = np.around(st.t.interval(confidence=0.95, df=len(data)-1, loc=np.mean(data), scale=st.sem(data)), 2)
   return f_list
+
+#based off of https://mclguide.readthedocs.io/en/latest/sklearn/clusterdim.html
+
+
+def pca(list_of_vibe_data):
+  """ 
+    expect a list of len N vibe data. Each vibe data has 8 points:
+     validated_songs, data, target and strings.
+     recommended_songs, data, target and strings
+  """
+
+  num_of_vibes =  len(list_of_vibe_data)
+  
+  x =[]
+  y = []
+  songnames = []
+  for vibe_data in list_of_vibe_data:
+   
+  
+    y += vibe_data[1] + vibe_data[5]
+    x += vibe_data[2] + vibe_data[6]
+    songnames +=  vibe_data[3] + vibe_data[7]
+
+ 
+  x=np.array(x)
+  y=np.array(y) 
+  songnames=np.array(songnames)
+
+
+  # Normalize Features to -1, 1, then Normalize Members to unit length, the
+  # Dimesionality reduction to 2
+  T0 = preprocessing.RobustScaler().fit_transform(x)
+  T1 = preprocessing.Normalizer().fit_transform(T0)
+  pca_model = PCA(n_components=2)
+  T2 = pca_model.fit_transform(T1) # fit the model
+
+
+  # store the values of PCA component in variable: for easy writing
+  xvector = pca_model.components_[0] * max(T2[:,0])
+  yvector = pca_model.components_[1] * max(T2[:,1])
+
+  df = pd.DataFrame(x, columns = chatspot.get_features())
+  df["vibe"] = y
+  df["songnames"] = songnames
+  df["comp-1"] = T2[:, 0]
+  df["comp-2"] = T2[:, 1]
+
+
+  rbf = GaussianProcessClassifier(1.0 * RBF(1.0))
+  rbf.fit(T2,y)
+
+  x_min, x_max = -1, 1.1
+  y_min, y_max =-1, 1.1
+
+  h = .01
+  xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+  y_ = np.arange(y_min, y_max, h)
+  Z = rbf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+  label_dict = {}
+  n_colors = 1
+  for label in y:
+    if label not in label_dict:
+      label_dict[label] = n_colors - 0.1
+      n_colors += 1
+
+  Z = np.array([label_dict[ele] for ele in Z])
+  Z = Z.reshape(xx.shape)
+
+  # Don't ask me why this works... at least for 4 colors
+  colormap = px.colors.sample_colorscale("rainbow", [n/(n_colors -1) for n in range(n_colors)])[1:]
+  heatmap = go.Heatmap(x=xx[0],
+                    y=y_, 
+                    z=Z,
+                    zmin=0,
+                    zmax=(n_colors-1),
+                    hoverinfo='skip',
+                    colorscale="rainbow",
+                    showscale=False)
+
+
+  title='Spotify Feature PCA projection'
+  hover_data={'vibe': False, 'comp-1':False, 'comp-2':False}
+  for f in chatspot.get_features():
+    hover_data[f]=True
+
+  fig = px.scatter(df, hover_name='songnames', hover_data=hover_data, title=title,
+                 x="comp-1", y="comp-2", color='vibe', width=1000, height=800,
+                 color_discrete_sequence=colormap)
+  fig.update_traces(marker=dict(size=6, line=dict(width=1, color='black')))
+ 
+  fig.add_trace(heatmap)
+
+  fig.update_layout(title={'font_family': "Arial",
+                         'x':0.45,
+                         'y': 0.92,    
+                         'xanchor': 'center' })
+
+
+  fig.update_yaxes(tickvals=[], showgrid=False, visible=False)
+  fig.update_xaxes(tickvals=[], showgrid=False, visible=False)
+
+  dim_sizes = []
+  for i in range(len(chatspot.get_features())):
+    dim_sizes.append(math.sqrt(pow(xvector[i],2) + pow(yvector[i],2)))
+    dim_sizes = sorted(dim_sizes, reverse=True)
+  third = dim_sizes[3]
+
+  arrowsize = 1
+  arrowhead = 1
+  arrowscale = 1
+  for i, feature in enumerate(chatspot.get_features()):
+        if math.sqrt(pow(xvector[i],2) + pow(yvector[i],2)) < third:
+          continue
+        fig.add_annotation(
+            ax=0, ay=0,
+            axref="x", ayref="y",
+            x=xvector[i]*arrowscale,
+            y=yvector[i]*arrowscale,
+            showarrow=True,
+            arrowsize=arrowsize,
+            arrowhead=arrowhead,
+            xanchor="right",
+            yanchor="top"
+        )
+        fig.add_annotation(
+            x=xvector[i]*arrowscale,
+            y=yvector[i]*arrowscale,
+            ax=0, ay=0,
+            xanchor="center",
+            yanchor="bottom",
+            text=feature,
+            bgcolor="white",
+            yshift=5,
+        )
+
+  return fig
+ 
