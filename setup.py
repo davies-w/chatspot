34231ff744192aabfcb8cc8758cb27c942af5571
Winton Davies <wdavies@cs.stanford.edu>
2023-06-17 18:17:58 -0700

    Update setup.py
    added pyyaml

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         url='git@github.com:davies-w/chatspot.git',
         author='Winton Davies',
         author_email='wdavies@cs.stanford.edu',
-        install_requires=["openai", "spotipy"],
+        install_requires=["openai", "spotipy", "pyyaml"],
         packages=['chatspot'],
         zip_safe=True
     )
